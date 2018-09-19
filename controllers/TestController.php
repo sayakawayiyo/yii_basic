9e5dc5393492da321bec1d49ae151919021acb89
wangli <LPzNbskF>
2018-09-19 19:03:54 +0800

    test:测试上传

---
+++ controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/9/19
+ * Time: 19:03
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class TestController extends Controller
+{
+	
+}
