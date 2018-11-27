0bacbc7f95bb58e4fbb6af535e02fd7bdc0a6a1b
wangli <LPzNbskF>
2018-11-27 23:48:50 +0800

    style: 测试样式

---
+++ web/test.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>madolche</title>
+    <link type="text/css" rel="stylesheet" href="" media="print and (max-devie-width:480px)">
+    <style>
+        p {
+            color: #cbb716;
+            font-weight: bolder;
+            font-size: 20px;
+            text-decoration: underline brown;
+            font-family: sans-serif;
+            line-height: 50px;
+            background: #2aabd2;
+            font-style: unset;
+        }
+
+        img {
+            max-width: 100px;
+            margin-left: 20px;
+            border: #b92c28 solid 6px;
+        }
+
+        @media screen and (max-width: 1000px) {
+            .first {
+                border: dotted 16px rebeccapurple;
+            }
+
+            .second {
+                border: ridge 16px rebeccapurple;
+            }
+        }
+
+        body {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<h1 title="大大大标题">h1</h1>
+<h2>h2</h2>
+<h3>h3</h3>
+<h4>h4</h4>
+<h5>h5</h5>
+<h6>h6</h6>
+<p class="second first">test</p>
+<p>test</p>
+<q>madolch</q>
+<blockquote title="这里是块引用">madolche</blockquote>
+&amp;&quot;&quot;&lt;&gt;&copy;&times;&divide;
+<a href=""><img src="./img/tohsaka.jpg" alt="测试加载不出来" title="fate_stay_night"></a>
+<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+</ul>
+<ol>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+</ol>
+</body>
+</html>
